2a5b4d8a19a52b5fd88f55416565b45b22a00b49
Jérémie Nuel <jeremie.nuel@pm.me>
2026-03-03 11:39:58 +0100

    julie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-let table = new DataTable("#content");
+// let table = new DataTable("#content");
+
+
+$(document).ready(function () {
+
+    $("#content").DataTable({
+        paging: false,
+        language: {
+            "decimal":        "",
+            "emptyTable":     "Pas de mémoires disponibles",
+            "info":           "Affichage _START_ de _END_ de _TOTAL_ mémoires",
+            "infoEmpty":      "Affichage 0 de 0 de 0 entrées",
+            "infoFiltered":   "(filtre de _MAX_ d'entrées totales)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Affiche _MENU_ mémoires",
+            "loadingRecords": "Chargement...",
+            "processing":     "",
+            "search":         "Recherche:",
+            "zeroRecords":    "Pas de mémoires disponibles",
+            "paginate": {
+                "first":      "Premier",
+                "last":       "Dernier",
+                "next":       "Suivant",
+                "previous":   "Précédent"
+            },
+            "aria": {
+                "orderable":  "Arrange l'ordre à partir de cette colonne",
+                "orderableReverse": "Arrange l'ordre inverse à partir de cette colonne"
+            }
+        }
+    })
+
+})
